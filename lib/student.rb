880298be735f463d2a830ef54c5c1456b5a5d143
jmoypace <jmoypace@gmail.com>
2018-08-01 21:39:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,8 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student_hash|
+        Student.new(student_hash)
   end
 
   def add_student_attributes(attributes_hash)
@@ -20,6 +21,6 @@ class Student
   end
 
   def self.all
-
+ @@all
   end
 end
